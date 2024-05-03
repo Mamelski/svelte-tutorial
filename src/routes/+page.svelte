07813095bc80883834e8e16aab298ff3378edecb
Jakub Mamelski <jakub.mamelski@outlook.com>
2024-05-03 20:58:33 +0200

    routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav>
+	<a href="/">home</a>
+	<a href="/about"> about</a>
+</nav>
+
+<h1>Home</h1>
+
+<p>This is the home page</p>
